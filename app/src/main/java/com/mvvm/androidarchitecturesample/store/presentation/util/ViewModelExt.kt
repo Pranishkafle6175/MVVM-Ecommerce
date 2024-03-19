e9afeed5fd4189c91f273e75f8559268a8ea376f
Pranish <pranishkafle1@gmail.com>
2024-03-19 07:45:49 +0545

    Extension function for sendevent EventBus

---
+++ app/src/main/java/com/mvvm/androidarchitecturesample/store/presentation/util/ViewModelExt.kt
@@ -0,0 +1,16 @@
+package com.mvvm.androidarchitecturesample.store.presentation.util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mvvm.androidarchitecturesample.store.util.EventBus
+import kotlinx.coroutines.launch
+
+
+//This function is written as a extension function because to achieve encapsulation . I fwe have to modify any logic for event bus we can modify in
+//the EventBus we dont have to change the implementation as this fucntion again calls the EventBus and same
+//sendevent will be called
+fun ViewModel.sendevent(event:Any){
+    viewModelScope.launch {
+        EventBus.sendEvent(event)
+    }
+}
